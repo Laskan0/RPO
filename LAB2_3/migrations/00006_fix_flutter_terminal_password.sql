d0dc20ee0efef0a03a717ddb156cb5bfaa6ad839
Laskano <laskan0@Laskano--MacBook-Air.local>
2026-05-29 17:34:48 +0300

    Удалил ошибочный проект

--- LAB2_3/migrations/00006_fix_flutter_terminal_password.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
-UPDATE users
-SET password_hash = '$2b$12$RQeuahHrQC9ikp.yenoLq.fFK/4J0mWyEAchr2Hyv4neRvDs8pCGC'
-WHERE login = 'flutter_terminal';
-
--- +goose Down
-UPDATE users
-SET password_hash = '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy'
-WHERE login = 'flutter_terminal';
--- LAB2_3/scripts/update-go-sum.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-docker run --rm \
-    -v "$(pwd)":/src \
-    -w /src \
-    golang:1.24.2-alpine \
-    go mod tidy
